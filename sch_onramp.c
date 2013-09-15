913431ed63e94bf44e3cb25b13ce0585fa0111ef
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-09-15 15:53:54 -0400

    Renaming part 5: flow -> client_queue

--- sch_onramp.c
+++ sch_onramp.c
@@ -19,7 +19,7 @@ struct onramp_client_queue {
 	struct sk_buff	  *tail;
 	struct list_head  pkt_chain;
 	int		  deficit;
-	u32		  dropped; /* number of drops (or ECN marks) on this flow */
+	u32		  dropped; /* number of drops on this client */
 }; /* please try to keep this structure <= 64 bytes */
 
 struct onramp_sched_data {
@@ -57,24 +57,24 @@ static unsigned int onramp_classify(struct sk_buff *skb, struct Qdisc *sch,
 /* helper functions : might be changed when/if skb use a standard list_head */
 
 /* remove one skb from head of slot queue */
-static inline struct sk_buff *dequeue_head(struct onramp_client_queue *flow)
+static inline struct sk_buff *dequeue_head(struct onramp_client_queue *client_queue)
 {
-	struct sk_buff *skb = flow->head;
+	struct sk_buff *skb = client_queue->head;
 
-	flow->head = skb->next;
+	client_queue->head = skb->next;
 	skb->next = NULL;
 	return skb;
 }
 
-/* add skb to flow queue (tail add) */
-static inline void flow_queue_add(struct onramp_client_queue *flow,
-				  struct sk_buff *skb)
+/* add skb to client_queue (tail add) */
+static inline void client_queue_add(struct onramp_client_queue *client_queue,
+				    struct sk_buff *skb)
 {
-	if (flow->head == NULL)
-		flow->head = skb;
+	if (client_queue->head == NULL)
+		client_queue->head = skb;
 	else
-		flow->tail->next = skb;
-	flow->tail = skb;
+		client_queue->tail->next = skb;
+	client_queue->tail = skb;
 	skb->next = NULL;
 }
 
@@ -83,9 +83,9 @@ static unsigned int onramp_drop(struct Qdisc *sch)
 	struct onramp_sched_data *q = qdisc_priv(sch);
 	struct sk_buff *skb;
 	unsigned int maxbacklog = 0, idx = 0, i, len;
-	struct onramp_client_queue *flow;
+	struct onramp_client_queue *client_queue;
 
-	/* Queue is full! Find the fat flow and drop packet from it.
+	/* Queue is full! Find the fat client and drop packet from it.
 	 * This might sound expensive, but with 1024 queues, we scan
 	 * 4KB of memory, and we dont need to handle a complex tree
 	 * in fast path (packet queue/enqueue) with many cache misses.
@@ -96,15 +96,15 @@ static unsigned int onramp_drop(struct Qdisc *sch)
 			idx = i;
 		}
 	}
-	flow = &q->queue_table[idx];
-	skb = dequeue_head(flow);
+	client_queue = &q->queue_table[idx];
+	skb = dequeue_head(client_queue);
 	len = qdisc_pkt_len(skb);
 	q->backlogs[idx] -= len;
 	kfree_skb(skb);
 	sch->q.qlen--;
 	sch->qstats.drops++;
 	sch->qstats.backlog -= len;
-	flow->dropped++;
+	client_queue->dropped++;
 	return idx;
 }
 
@@ -112,7 +112,7 @@ static int onramp_enqueue(struct sk_buff *skb, struct Qdisc *sch)
 {
 	struct onramp_sched_data *q = qdisc_priv(sch);
 	unsigned int idx;
-	struct onramp_client_queue *flow;
+	struct onramp_client_queue *client_queue;
 	int uninitialized_var(ret);
 
 	idx = onramp_classify(skb, sch, &ret);
@@ -124,23 +124,23 @@ static int onramp_enqueue(struct sk_buff *skb, struct Qdisc *sch)
 	}
 	idx--;
 
-	flow = &q->queue_table[idx];
-	flow_queue_add(flow, skb);
+	client_queue = &q->queue_table[idx];
+	client_queue_add(client_queue, skb);
 	q->backlogs[idx] += qdisc_pkt_len(skb);
 	sch->qstats.backlog += qdisc_pkt_len(skb);
 
-	if (list_empty(&flow->pkt_chain)) {
-		list_add_tail(&flow->pkt_chain, &q->active_clients);
+	if (list_empty(&client_queue->pkt_chain)) {
+		list_add_tail(&client_queue->pkt_chain, &q->active_clients);
 		q->clients_so_far++;
-		flow->deficit = q->quantum;
-		flow->dropped = 0;
+		client_queue->deficit = q->quantum;
+		client_queue->dropped = 0;
 	}
 	if (++sch->q.qlen <= sch->limit)
 		return NET_XMIT_SUCCESS;
 
 	q->drop_overlimit++;
 	/* Return Congestion Notification only if we dropped a packet
-	 * from this flow.
+	 * from this client_queue.
 	 */
 	if (onramp_drop(sch) == idx)
 		return NET_XMIT_CN;
@@ -150,17 +150,17 @@ static int onramp_enqueue(struct sk_buff *skb, struct Qdisc *sch)
 	return NET_XMIT_SUCCESS;
 }
 
-/* This is the function to dequeue a packet from a specific flow.
+/* This is the function to dequeue a packet from a specific client_queue.
  * Anirudh: Qdisc::backlog was handled in codel, but we are ignoring it completely.
  */
-static struct sk_buff *dequeue_from_flow(struct Qdisc *sch, struct onramp_client_queue* flow)
+static struct sk_buff *dequeue_from_client_queue(struct Qdisc *sch, struct onramp_client_queue* client_queue)
 {
 	struct onramp_sched_data *q = qdisc_priv(sch);
 	struct sk_buff *skb = NULL;
 
-	if (flow->head) {
-		skb = dequeue_head(flow);
-		q->backlogs[flow - q->queue_table] -= qdisc_pkt_len(skb);
+	if (client_queue->head) {
+		skb = dequeue_head(client_queue);
+		q->backlogs[client_queue - q->queue_table] -= qdisc_pkt_len(skb);
 		sch->q.qlen--;
 	}
 	return skb;
@@ -170,7 +170,7 @@ static struct sk_buff *onramp_dequeue(struct Qdisc *sch)
 {
 	struct onramp_sched_data *q = qdisc_priv(sch);
 	struct sk_buff *skb;
-	struct onramp_client_queue *flow;
+	struct onramp_client_queue *client_queue;
 	struct list_head *head;
 
 	printk("Entering onramp_dequeue\n");
@@ -179,27 +179,27 @@ begin:
 	if (list_empty(head)) {
 		return NULL;
 	}
-	flow = list_first_entry(head, struct onramp_client_queue, pkt_chain);
+	client_queue = list_first_entry(head, struct onramp_client_queue, pkt_chain);
 
-	if (flow->deficit <= 0) {
-		flow->deficit += q->quantum;
-		list_move_tail(&flow->pkt_chain, &q->active_clients);
+	if (client_queue->deficit <= 0) {
+		client_queue->deficit += q->quantum;
+		list_move_tail(&client_queue->pkt_chain, &q->active_clients);
 		goto begin;
 	}
 
 
-	skb = dequeue_from_flow(sch, flow);
+	skb = dequeue_from_client_queue(sch, client_queue);
 
-	/* Anirudh: flow->dropped doesn't need to be updated
+	/* Anirudh: client_queue->dropped doesn't need to be updated
 	   because we no longer drop here */
 
 	if (!skb) {
-		/* Remove the flow from the list */
-		list_del_init(&flow->pkt_chain);
+		/* Remove the client_queue from the list */
+		list_del_init(&client_queue->pkt_chain);
 		goto begin;
 	}
 	qdisc_bstats_update(sch, skb);
-	flow->deficit -= qdisc_pkt_len(skb);
+	client_queue->deficit -= qdisc_pkt_len(skb);
 	/* We cant call qdisc_tree_decrease_qlen() if our qlen is 0,
 	 * or HTB crashes. Defer it for next round.
 	 */
@@ -278,9 +278,9 @@ static int onramp_init(struct Qdisc *sch, struct nlattr *opt)
 			return -ENOMEM;
 		}
 		for (i = 0; i < q->max_clients; i++) {
-			struct onramp_client_queue *flow = q->queue_table + i;
+			struct onramp_client_queue *client_queue = q->queue_table + i;
 
-			INIT_LIST_HEAD(&flow->pkt_chain);
+			INIT_LIST_HEAD(&client_queue->pkt_chain);
 		}
 	}
 
