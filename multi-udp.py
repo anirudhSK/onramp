cf75eb10f963427c930973fb5fc53e97b47cb1b7
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-09-16 22:50:29 -0400

    Flood UDP packets at more regular intervals.

--- multi-udp.py
+++ multi-udp.py
@@ -13,7 +13,7 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind(('10.0.0.1',0))
 while(1):
-	sleep(1)
+	sleep(0.01)
 	UDP_REL_PORT = UDP_REL_PORT + 1
 	print UDP_REL_PORT
 	sock.sendto(MESSAGE, (UDP_IP, 5000 + (UDP_REL_PORT + 1)%10000))
