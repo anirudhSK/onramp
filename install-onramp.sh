221864635ec838937756e687e18c49f89ce43901
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-09-26 15:11:02 -0400

    Attach a Rate Limiter to actually see queues in onramp.

--- install-onramp.sh
+++ install-onramp.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 set -e
+make -C /lib/modules/`uname -r`/build SUBDIRS=$PWD clean
 make -C /lib/modules/`uname -r`/build SUBDIRS=$PWD modules
 
 set +e
@@ -8,4 +9,7 @@ sudo rmmod  ./onramp.ko
 set -e
 
 sudo insmod ./onramp.ko
-sudo tc qdisc add dev ingress root onramp
+sudo ifconfig ingress 10.0.0.1
+set -x; for i in `echo "2 3 4 5 6 7 8 9"`; do sudo arp -s 10.0.0.$i $i$i:$i$i:$i$i:$i$i:$i$i:$i$i ; done; set +x
+sudo tc qdisc add dev ingress root handle 1:0 tbf rate 256kbit buffer 1600 limit 3
+sudo tc qdisc add dev ingress parent 1:1 handle 10: onramp
